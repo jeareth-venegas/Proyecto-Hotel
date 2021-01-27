1fcb77dbeeceb41e2003b46b6d5836232263da58
JeampauÃl Salazar Castillo <jpaulsalazar28@gmail.com>
2021-01-27 00:33:32 -0600

    se termino la pagina de destinos

--- js/script.js
+++ js/script.js
@@ -3,6 +3,11 @@ const enlaces = document.querySelectorAll('.tabs-menu');
 
 const contenido = document.querySelectorAll('.tab-content');
 
+// Variables que obtinenen los enlaces de miniaturas y el div contenedor con la imagen en grande
+const links = document.querySelectorAll('.miniature');
+
+const bigImage = document.getElementById('bigImage');
+
 for (let i = 0; i < contenido.length; i++) {
   contenido[i].classList.add('js-content-hidden');
 }
@@ -27,5 +32,24 @@ for (let i = 0; i < enlaces.length; i++) {
       enlaces[i].classList.remove('js-active');
     }
     event.currentTarget.classList.add('js-active');
+  });
+}
+
+// Funcion principal de la Galeria
+for (let i = 0; i < links.length; i++) {
+  links[i].addEventListener('click', (event) => {
+    event.preventDefault();
+    const url = event.currentTarget.getAttribute('src');
+    bigImage.src = `${url}`;
+    for (let i = 0; i < links.length; i++) {
+      links[i].classList.add('img-content-hidden');
+      if (links[i].getAttribute('src') === url) {
+        links[i].classList.remove('img-content-hidden');
+      }
+    }
+    for (let i = 0; i < bigImage.length; i++) {
+      bigImage[i].classList.remove('img-active');
+    }
+    event.currentTarget.classList.add('img-active');
   });
 }
